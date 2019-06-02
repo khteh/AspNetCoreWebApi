093346d9353c7fe1595e5d34788226c4dab5c25a
Teh Kok How <funcoolgeek@gmail.com>
2019-06-02 18:13:49 +0800

    Clean up

--- src/Web.Api.Core/Dto/Error.cs
+++ src/Web.Api.Core/Dto/Error.cs
@@ -1,15 +1,10 @@
-﻿
-
-using System;
-
+﻿using System;
 namespace Web.Api.Core.Dto
 {
-    [Serializable]
     public sealed class Error
     {
         public string Code { get; }
         public string Description { get; }
-
         public Error(string code, string description)
         {
             Code = code;
