c77521c42599c5589089fc5e7cf74bd8a6c1e7e3
Teh Kok How <khteh@KHTeh-T490.biz4x.com>
2019-06-17 09:15:01 +0800

    Add Request path base to work with kubernetes ingress

--- src/Web.Api/Startup.cs
+++ src/Web.Api/Startup.cs
@@ -193,6 +193,11 @@ namespace Web.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.Use((context, next) =>
+            {
+                context.Request.PathBase = new PathString("/apistarter");
+                return next();
+            });
             app.UseExceptionHandler(
                 builder =>
                 {
