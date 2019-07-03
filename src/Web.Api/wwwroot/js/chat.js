2beca6f17dd8ffc40261d18688722820aa5ba69d
Teh Kok How <funcoolgeek@gmail.com>
2019-07-03 22:18:04 +0800

    Add login to chat.js

--- src/Web.Api/wwwroot/js/chat.js
+++ src/Web.Api/wwwroot/js/chat.js
@@ -1,8 +1,10 @@
 "use strict";
+var login = true;
+var token = null;
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub", {
     skipNegotiation: true,
     transport: signalR.HttpTransportType.WebSockets,
-    accessTokenFactory: () => this.getToken()//document.getElementById("tokenInput").value
+    accessTokenFactory: () => token
 }).configureLogging(signalR.LogLevel.Trace).build();
 // Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
@@ -34,6 +36,35 @@ document.getElementById("sendButton").addEventListener("click", function (event)
         connection.invoke("ReceiveMessage", message).catch(function (err) { return console.error(err.toString()); });
     event.preventDefault();
 });
-function getToken() {
-    return document.getElementById("tokenInput").value
-}
+document.getElementById("loginButton").addEventListener("click", function (event) {
+    var btn = document.getElementById("loginButton").value;
+    if (btn === "Login") {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        if (username && password) {
+            $.ajax({
+                url: "/api/auth/login",
+                type: "POST",
+                contentType: "application/json",
+                dataType: "json",
+                data: { UserName: username, Password: password, RememberMe: true },
+                processData: false,
+                success: function (blob) {
+                    console.log(data);
+                    token = data.accessToken.token;
+                },
+                error: function (data) {
+                    console.error("Login failed!" + JSON.stringify(data));
+                }
+            });
+        }
+    } else if (btn === "Logout") {
+        this.token = null;
+        document.getElementById("sendButton").disabled = true;
+        connection.stop().then(function () {
+        }).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    event.preventDefault();
+});
