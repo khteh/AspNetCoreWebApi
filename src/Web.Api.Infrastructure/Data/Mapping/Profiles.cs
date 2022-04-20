4bf8fc44d1d719b717dcf84fdaf9ba9dc7e7fa83
Teh Kok How <funcoolgeek@gmail.com>
2022-04-20 15:46:05 +0800

    (1) Upgrade packages to the latest versions. (2) Remove obsolete automapper ForAllOtherMembers

--- src/Web.Api.Infrastructure/Data/Mapping/Profiles.cs
+++ src/Web.Api.Infrastructure/Data/Mapping/Profiles.cs
@@ -7,15 +7,13 @@ public class IdentityProfile : Profile
     public IdentityProfile()
     {
         CreateMap<User, AppUser>().ConstructUsing(u => new AppUser(u.UserName, u.Email, u.FirstName, u.LastName))
-                                        .ForMember(au => au.Id, opt => opt.Ignore())
-                                        .ForAllOtherMembers(o => o.Ignore());
+                                        .ForMember(au => au.Id, opt => opt.Ignore());
         CreateMap<AppUser, User>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.IdentityId, o => o.MapFrom(src => src.Id))
                 .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
                 .ForMember(dest => dest.FirstName, o => o.MapFrom(src => src.FirstName))
-                .ForMember(dest => dest.LastName, o => o.MapFrom(src => src.LastName))
-                .ForAllOtherMembers(opt => opt.Ignore());
+                .ForMember(dest => dest.LastName, o => o.MapFrom(src => src.LastName));
     }
 }
