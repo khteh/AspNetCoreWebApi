813324eef6d69d837e87183561d31f6fb9f102ae
Teh Kok How <funcoolgeek@gmail.com>
2022-09-24 15:08:38 +0800

    Use inline property assignment

--- src/Web.Api.Core/DTO/GatewayResponses/Repositories/CreateUserResponse.cs
+++ src/Web.Api.Core/DTO/GatewayResponses/Repositories/CreateUserResponse.cs
@@ -3,8 +3,5 @@ namespace Web.Api.Core.DTO.GatewayResponses.Repositories;
 public sealed class CreateUserResponse : BaseGatewayResponse
 {
     public string Id { get; }
-    public CreateUserResponse(string id, bool success = false, List<Error> errors = null) : base(success, errors)
-    {
-        Id = id;
-    }
+    public CreateUserResponse(string id, bool success = false, List<Error> errors = null) : base(success, errors) => Id = id;
 }
