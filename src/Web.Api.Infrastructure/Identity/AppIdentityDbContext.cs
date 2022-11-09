980019767f0b829c8ba7b82ecfd8db2aa070c5c9
Teh Kok How <funcoolgeek@gmail.com>
2022-11-09 09:53:18 +0800

    Upgrade to .Net 7

--- src/Web.Api.Infrastructure/Identity/AppIdentityDbContext.cs
+++ src/Web.Api.Infrastructure/Identity/AppIdentityDbContext.cs
@@ -6,31 +6,5 @@ public class AppIdentityDbContext : IdentityDbContext<AppUser>
     public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
     {
     }
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-#if false
-        builder.Entity<IdentityUser>(entity => entity.Property(m => m.Id).HasMaxLength(85));
-        builder.Entity<IdentityUser>(entity => entity.Property(m => m.NormalizedEmail).HasMaxLength(85));
-        builder.Entity<IdentityUser>(entity => entity.Property(m => m.NormalizedUserName).HasMaxLength(85));
-        builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id).HasMaxLength(85));
-        builder.Entity<IdentityRole>(entity => entity.Property(m => m.NormalizedName).HasMaxLength(85));
-
-        builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.LoginProvider).HasMaxLength(85));
-        builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.ProviderKey).HasMaxLength(85));
-        builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.UserId).HasMaxLength(85));
-        builder.Entity<IdentityUserRole<string>>(entity => entity.Property(m => m.UserId).HasMaxLength(85));
-
-        builder.Entity<IdentityUserRole<string>>(entity => entity.Property(m => m.RoleId).HasMaxLength(85));
-
-        builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.UserId).HasMaxLength(85));
-        builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.LoginProvider).HasMaxLength(85));
-        builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.Name).HasMaxLength(85));
-
-        builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.Id).HasMaxLength(85));
-        builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.UserId).HasMaxLength(85));
-        builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.Id).HasMaxLength(85));
-        builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.RoleId).HasMaxLength(85));
-#endif
-    }
+    protected override void OnModelCreating(ModelBuilder builder) => base.OnModelCreating(builder);
 }
