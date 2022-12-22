a7e0d051e4f06d84fcbed1c7d48cf807701e5b0c
Kok How, Teh <funcoolgeek@gmail.com>
2022-12-22 10:38:33 +0800

    Persists data-protection keys to redis cache

--- src/Web.Api.Infrastructure/InfrastructureServices.cs
+++ src/Web.Api.Infrastructure/InfrastructureServices.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using StackExchange.Redis;
 using Web.Api.Core.Configuration;
 using Web.Api.Core.Interfaces.Gateways.Repositories;
 using Web.Api.Core.Interfaces.Services;
@@ -12,6 +13,8 @@ using Web.Api.Infrastructure.Data;
 using Web.Api.Infrastructure.Data.Repositories;
 using Web.Api.Infrastructure.Identity;
 using Web.Api.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.StackExchangeRedis;
 namespace Microsoft.Extensions.DependencyInjection;
 public static class InfrastructureServices
 {
@@ -26,11 +29,15 @@ public static class InfrastructureServices
         service.AddDbContextPool<AppIdentityDbContext>(options => options.UseNpgsql(configuration.GetConnectionString(isIntegrationTest ? "IntegrationTests" : "Default"), b => b.MigrationsAssembly("Web.Api.Infrastructure")))
                     .AddDbContextPool<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString(isIntegrationTest ? "IntegrationTests" : "Default"), b => b.MigrationsAssembly("Web.Api.Infrastructure")));
         if (!isIntegrationTest && env.IsProduction())
+        {
             service.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = configuration["RedisCache:Connection"];
                 options.InstanceName = configuration["RedisCache:InstanceName"];
             });
+            var redis = ConnectionMultiplexer.Connect(configuration["RedisCache:Connection"]);
+            service.AddDataProtection().PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
+        }
         return service;
     }
 }
