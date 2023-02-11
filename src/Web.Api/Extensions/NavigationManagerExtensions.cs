b2856dd24e5929a1fc0fa3c368c11947e9e74d75
Kok How, Teh <funcoolgeek@gmail.com>
2023-02-11 15:04:59 +0800

    Remove empty lines

--- src/Web.Api/Extensions/NavigationManagerExtensions.cs
+++ src/Web.Api/Extensions/NavigationManagerExtensions.cs
@@ -7,7 +7,6 @@ public static class NavigationManagerExtensions
     public static bool TryGetQueryString<T>(this NavigationManager navManager, string key, out T value)
     {
         var uri = navManager.ToAbsoluteUri(navManager.Uri);
-
         if (QueryHelpers.ParseQuery(uri.Query).TryGetValue(key, out var valueFromQueryString))
         {
             if (typeof(T) == typeof(int) && int.TryParse(valueFromQueryString, out var valueAsInt))
@@ -15,13 +14,11 @@ public static class NavigationManagerExtensions
                 value = (T)(object)valueAsInt;
                 return true;
             }
-
             if (typeof(T) == typeof(string))
             {
                 value = (T)(object)valueFromQueryString.ToString();
                 return true;
             }
-
             if (typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out var valueAsDecimal))
             {
                 value = (T)(object)valueAsDecimal;
