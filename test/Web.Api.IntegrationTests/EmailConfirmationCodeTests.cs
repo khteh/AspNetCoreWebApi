ae06c6d20117a9804a25cfbf84e64688cc29240e
Kok How, Teh <funcoolgeek@gmail.com>
2023-02-23 14:40:26 +0800

    Add EmailConfirmationTokenShoudNOTMatchTest

--- test/Web.Api.IntegrationTests/EmailConfirmationCodeTests.cs
+++ test/Web.Api.IntegrationTests/EmailConfirmationCodeTests.cs
@@ -34,4 +34,23 @@ public class EmailConfirmationCodeTests
             Assert.True(identityResult.Succeeded);
         }
     }
+    [Fact]
+    public async Task EmailConfirmationTokenShoudNOTMatchTest()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            UserManager<AppUser> userManager = scopedServices.GetRequiredService<UserManager<AppUser>>();
+            Assert.NotNull(userManager);
+            AppUser user = await userManager.FindByNameAsync("mickeymouse");
+            Assert.NotNull(user);
+            string code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            AppUser user1 = await userManager.FindByNameAsync("user1");
+            Assert.NotNull(user1);
+            IdentityResult identityResult = await userManager.ConfirmEmailAsync(user1, code);
+            Assert.True(identityResult.Succeeded);
+        }
+    }
 }
