cacdae697a90378c936d59c45beb7da90c655a67
Kok How, Teh <funcoolgeek@gmail.com>
2023-02-23 14:33:36 +0800

    Add Email confirmation token test

---
+++ test/Web.Api.IntegrationTests/EmailConfirmationCodeTests.cs
@@ -0,0 +1,37 @@
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Api.Infrastructure.Identity;
+using Xunit;
+
+namespace Web.Api.IntegrationTests;
+[Collection("Tests Collection")]
+public class EmailConfirmationCodeTests
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+    public EmailConfirmationCodeTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        factory.InitDB();
+    }
+    [Fact]
+    public async Task EmailConfirmationTokenShoudMatchTest()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            UserManager<AppUser> userManager = scopedServices.GetRequiredService<UserManager<AppUser>>();
+            Assert.NotNull(userManager);
+            AppUser user = await userManager.FindByNameAsync("mickeymouse");
+            Assert.NotNull(user);
+            string code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            IdentityResult identityResult = await userManager.ConfirmEmailAsync(user, code);
+            Assert.True(identityResult.Succeeded);
+        }
+    }
+}
