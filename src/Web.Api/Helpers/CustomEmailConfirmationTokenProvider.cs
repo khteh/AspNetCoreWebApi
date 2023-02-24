765098a370e18c37b794d3b613d863c014d2a491
Teh Kok How <funcoolgeek@gmail.com>
2023-02-24 14:11:10 +0800

    Add CustomEmailConfirmationTokenProvider and configure app cookie timeout

---
+++ src/Web.Api/Helpers/CustomEmailConfirmationTokenProvider.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Options;
+
+namespace Web.Api.Helpers;
+public class CustomEmailConfirmationTokenProvider<TUser>
+                              :  DataProtectorTokenProvider<TUser> where TUser : class
+{
+    public CustomEmailConfirmationTokenProvider(
+        IDataProtectionProvider dataProtectionProvider,
+        IOptions<EmailConfirmationTokenProviderOptions> options,
+        ILogger<DataProtectorTokenProvider<TUser>> logger)
+                                       : base(dataProtectionProvider, options, logger)
+    {
+
+    }
+}
+public class EmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
+{
+    public EmailConfirmationTokenProviderOptions()
+    {
+        Name = "EmailDataProtectorTokenProvider";
+        TokenLifespan = TimeSpan.FromHours(4);
+    }
+}
