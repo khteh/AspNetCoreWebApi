8b93744f689e63fd9ed2d107e66edbd8d0638a90
Kok How, Teh <funcoolgeek@gmail.com>
2023-04-01 13:17:06 +0800

    email helper validation

--- src/Web.Api.Infrastructure/Email/EmailHelper.cs
+++ src/Web.Api.Infrastructure/Email/EmailHelper.cs
@@ -21,28 +21,33 @@ public class EmailHelper : IEmail
     public EmailHelper(ILogger<EmailHelper> logger, IOptions<EmailSettings> config, IWebHostEnvironment env) => (_logger, _config, _isProduction) = (logger, config.Value, env.IsProduction());
     public async Task<bool> SendEmailAsync(string recipientEmail, string subject, string textBody, string htmlBody)
     {
-        MimeMessage message = new MimeMessage();
-        message.From.Add(MailboxAddress.Parse(_config.UserName));
-        message.To.Add(MailboxAddress.Parse(recipientEmail));
-        message.Subject = subject;
-        BodyBuilder bodyBuilder = new BodyBuilder();
-        bodyBuilder.HtmlBody = htmlBody;
-        bodyBuilder.TextBody = textBody;
-        message.Body = bodyBuilder.ToMessageBody();
-        using (SmtpClient client = new SmtpClient())
+        if (EmailValidation.IsValidEmail(recipientEmail) && (!string.IsNullOrEmpty(textBody) || !string.IsNullOrEmpty(htmlBody)))
             try
             {
-                await client.ConnectAsync(_config.Server, _config.Port, _config.TLS);
-                await client.AuthenticateAsync(_config.UserName, _config.Password);
-                await client.SendAsync(message);
-                await client.DisconnectAsync(true);
-                _logger.LogInformation($"{nameof(SendEmailAsync)} Successfully sent email to {recipientEmail}");
+                MimeMessage message = new MimeMessage();
+                message.From.Add(MailboxAddress.Parse(_config.UserName));
+                message.To.Add(MailboxAddress.Parse(recipientEmail));
+                message.Subject = subject;
+                BodyBuilder bodyBuilder = new BodyBuilder();
+                bodyBuilder.HtmlBody = htmlBody;
+                bodyBuilder.TextBody = textBody;
+                message.Body = bodyBuilder.ToMessageBody();
+                using (SmtpClient client = new SmtpClient())
+                {
+                    await client.ConnectAsync(_config.Server, _config.Port, _config.TLS);
+                    await client.AuthenticateAsync(_config.UserName, _config.Password);
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                    _logger.LogInformation($"{nameof(SendEmailAsync)} Successfully sent email to {recipientEmail}");
+                }
             }
             catch (Exception e)
             {
                 _logger.LogCritical($"{nameof(SendEmailAsync)} Exception! {e}");
                 return false;
             }
+        else
+            _logger.LogError($"{nameof(SendEmailAsync)} Invalid email address: {recipientEmail} or empty message body!");
         return true;
     }
 
@@ -57,7 +62,7 @@ public class EmailHelper : IEmail
     }
     public async Task<bool> SendEmailAsync(Core.Domain.Email email)
     {
-        if (email != null && EmailValidation.IsValidEmail(email.Recipient))
+        if (email != null && EmailValidation.IsValidEmail(email.Recipient) && (!string.IsNullOrEmpty(email.HtmlBody) || !string.IsNullOrEmpty(email.TextBody)))
             try
             {
                 MimeMessage message = new MimeMessage();
@@ -84,7 +89,7 @@ public class EmailHelper : IEmail
                 _logger.LogCritical($"{nameof(SendEmailAsync)} Exception! {e}");
             }
         else
-            _logger.LogError($"{nameof(SendEmailAsync)} Invalid email address! {email.Recipient}");
+            _logger.LogError($"{nameof(SendEmailAsync)} Invalid email address: {email?.Recipient} or empty message body!");
         return false;
     }
 }
