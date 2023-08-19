a39855a614a473a094260f949db27f9d5a2c8c5e
Teh Kok How <funcoolgeek@gmail.com>
2023-08-19 16:08:12 +0800

    Add PhoneNumber to user.proto

--- src/Web.Api/Presenters/Grpc/GrpcProfile.cs
+++ src/Web.Api/Presenters/Grpc/GrpcProfile.cs
@@ -7,12 +7,13 @@ public class GrpcProfile : Profile
 {
     public GrpcProfile()
     {
-        CreateMap<Error, Identity.Error>().ConstructUsing(i => new Identity.Error() {Code = i.Code, Description = i.Description});
-        CreateMap<RefreshToken, Identity.RefreshToken>().ConstructUsing(i => new Identity.RefreshToken() {
-                Token = i.Token, 
-                UserId = i.UserId, 
-                RemoteIpAddress = i.RemoteIpAddress
-            }).ForMember(i => i.Expires, o => o.MapFrom(src => Timestamp.FromDateTimeOffset(src.Expires)));
+        CreateMap<Error, Identity.Error>().ConstructUsing(i => new Identity.Error() { Code = i.Code, Description = i.Description });
+        CreateMap<RefreshToken, Identity.RefreshToken>().ConstructUsing(i => new Identity.RefreshToken()
+        {
+            Token = i.Token,
+            UserId = i.UserId,
+            RemoteIpAddress = i.RemoteIpAddress
+        }).ForMember(i => i.Expires, o => o.MapFrom(src => Timestamp.FromDateTimeOffset(src.Expires)));
         CreateMap<User, Identity.User>()
                 .ForMember(i => i.Id, o => o.MapFrom(src => src.Id))
                 .ForMember(i => i.IdentityId, o => o.MapFrom(src => !string.IsNullOrEmpty(src.IdentityId) ? src.IdentityId : string.Empty))
@@ -20,6 +21,7 @@ public class GrpcProfile : Profile
                 .ForMember(i => i.LastName, o => o.MapFrom(src => !string.IsNullOrEmpty(src.LastName) ? src.LastName : string.Empty))
                 .ForMember(i => i.UserName, o => o.MapFrom(src => !string.IsNullOrEmpty(src.UserName) ? src.UserName : string.Empty))
                 .ForMember(i => i.Email, o => o.MapFrom(src => !string.IsNullOrEmpty(src.Email) ? src.Email : string.Empty))
+                .ForMember(i => i.PhoneNumber, o => o.MapFrom(src => !string.IsNullOrEmpty(src.PhoneNumber) ? src.PhoneNumber : string.Empty))
                 .ForMember(i => i.RefreshTokens, o => o.MapFrom(src => src.RefreshTokens));
     }
 }
