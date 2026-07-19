2b12616c1c46116e06b4d81c92eae12629ef9cdb
Kok How, Teh <funcoolgeek@gmail.com>
2026-07-19 14:03:01 +0800

    Add StackExchangeRedisCache service to Staging env

--- src/Web.Api.Infrastructure/InfrastructureServices.cs
+++ src/Web.Api.Infrastructure/InfrastructureServices.cs
@@ -51,7 +51,7 @@ public static class InfrastructureServices
         });
         service.AddTransient<CustomEmailConfirmationTokenProvider<AppUser>>();
         //if (!isIntegrationTest && env.IsProduction() && Assembly.GetEntryAssembly().GetName().Name.Equals("GetDocument.Insider")) // XXX: Temporary fix until https://github.com/dotnet/aspnetcore/issues/54698 is fixed
-        if (!isIntegrationTest && env.IsProduction()) // XXX: Temporary fix until https://github.com/dotnet/aspnetcore/issues/54698 is fixed
+        if (!isIntegrationTest && (env.IsProduction() || env.IsStaging())) // XXX: Temporary fix until https://github.com/dotnet/aspnetcore/issues/54698 is fixed
         {
             service.AddStackExchangeRedisCache(options =>
             {
