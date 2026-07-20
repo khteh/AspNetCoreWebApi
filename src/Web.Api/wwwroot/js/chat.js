b51569aa21763d5e7f62502397cef00564d79e27
Kok How, Teh <funcoolgeek@gmail.com>
2026-07-20 12:28:17 +0800

    Disable the send button on message sent

--- src/Web.Api/wwwroot/js/chat.js
+++ src/Web.Api/wwwroot/js/chat.js
@@ -50,6 +50,7 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     else
         connection.invoke("ReceiveMessage", message).catch(function (err) { return console.error(err.toString()); });
     document.getElementById("messageInput").value = "";
+    document.getElementById("sendButton").disabled = true;
     event.preventDefault();
 });
 document.getElementById("loginButton").addEventListener("click", function (event) {
